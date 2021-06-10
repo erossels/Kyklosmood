cb06f4d49b6f05fdad8bd3eda394c634d96e8a06
Eduardo Rossel <eduardorossel@outlook.cl>
2021-06-10 19:58:31 -0400

    question2

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -8,5 +8,8 @@ class HomeController < ApplicationController
     @answer.choice = params[:value]
     @answer.question_id = Question.all.last.id
     @answer.save
+
+    @answers = Answer.all.where(question_id: Question.all.last.id)
+
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :admins
   resources :options
   get 'home/index'
   resources :questions
   root 'home#index'
-  get 'home/thanks/:value', to: 'home#thanks', as: 'thanks'
+  get 'home/thanks', to: 'home#thanks', as: 'thanks'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
